912bb0fc4db31510fb22ce7a4fe3a20567e84dcf
Santhosh Adiga <santhoshadigau@SanthoshAdigas-MacBook-Pro.local>
2015-12-16 17:45:31 +0530

    added a project

---
+++ RecyclerView/app/src/main/java/santhoshadiga/com/recyclerview/MyActivity.java
@@ -0,0 +1,110 @@
+package santhoshadiga.com.recyclerview;
+
+import android.app.Activity;
+import android.graphics.Outline;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+
+
+public class MyActivity extends Activity {
+
+    private FrameLayout deleteBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        // Deletebar
+        deleteBar = (FrameLayout) findViewById(R.id.deleteBar);
+
+
+        // Fab button
+        int fabSize = getResources().getDimensionPixelSize(R.dimen.fab_size);
+        Outline fabOutline = new Outline();
+        fabOutline.setOval(0, 0, fabSize, fabSize);
+
+        View fabView = findViewById(R.id.fab_add);
+        fabView.setOutline(fabOutline);
+
+        // RecyclerView
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        // RecyclerView layout manager
+        final LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        // RecyclerView ItemDecoration (divider)
+        final RecyclerView.ItemDecoration itemDecoration = new SampleDivider(this);
+        recyclerView.addItemDecoration(itemDecoration);
+
+        // RecyclerView adapter
+        final SampleRecyclerAdapter sampleRecyclerAdapter = new SampleRecyclerAdapter();
+        recyclerView.setAdapter(sampleRecyclerAdapter);
+
+        // RecyclerView add element logic
+        fabView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int positionToAdd = mLayoutManager.findFirstCompletelyVisibleItemPosition();
+                sampleRecyclerAdapter.addItem(positionToAdd);
+            }
+        });
+
+        deleteBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int positionToRemove = mLayoutManager.findFirstCompletelyVisibleItemPosition();
+                sampleRecyclerAdapter.removeData(positionToRemove);
+                hideDeleteBar();
+            }
+        });
+
+        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(int i) {
+
+            }
+
+            @Override
+            public void onScrolled(int dx, int dy) {
+
+                if (dy > 0) {
+
+                    if (deleteBar.getVisibility() == View.VISIBLE)
+                        hideDeleteBar();
+
+                } else {
+
+                    if (deleteBar.getVisibility() == View.GONE)
+                        showDeleteBar();
+                }
+            }
+        });
+    }
+
+    private void showDeleteBar() {
+
+        deleteBar.startAnimation(AnimationUtils.loadAnimation(this,
+                R.anim.translate_up_on));
+
+        deleteBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideDeleteBar() {
+
+        deleteBar.startAnimation(AnimationUtils.loadAnimation(this,
+                R.anim.translate_up_off));
+
+        deleteBar.setVisibility(View.GONE);
+    }
+}
---
+++ RecyclerView/app/src/main/java/santhoshadiga/com/recyclerview/SampleRecyclerAdapter.java
@@ -0,0 +1,69 @@
+package santhoshadiga.com.recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class SampleRecyclerAdapter extends RecyclerView.Adapter<SampleRecyclerAdapter.ViewHolder> {
+
+    private final ArrayList<SampleModel> sampleData = DemoApp.getSampleData(20);
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parentViewGroup, int i) {
+
+        View rowView = LayoutInflater.from (parentViewGroup.getContext())
+            .inflate(R.layout.list_basic_item, parentViewGroup, false);
+
+        return new ViewHolder (rowView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+        final SampleModel rowData = sampleData.get(position);
+        viewHolder.textViewSample.setText(rowData.getSampleText());
+
+        viewHolder.itemView.setTag(rowData);
+    }
+
+
+    @Override
+    public int getItemCount() {
+
+        return sampleData.size();
+    }
+
+    public void removeData (int position) {
+
+        sampleData.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void addItem(int positionToAdd) {
+
+        sampleData.add(positionToAdd, new SampleModel("New element"));
+        notifyItemInserted(positionToAdd);
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView textViewSample;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            textViewSample = (TextView) itemView.findViewById(
+                R.id.textViewSample);
+        }
+    }
+
+}
